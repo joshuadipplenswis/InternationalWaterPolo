10ee346352e6c54c5bcc0ed0f39c327e192bb40a
joshuadipplenswis <joshua.dipple@nswis.com.au>
2025-08-19 15:07:19 +1000

    Commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+os.environ["STREAMLIT_DISABLE_WATCHDOG_WARNING"] = "true"
+os.environ["WATCHDOG_DISABLE_FILE_WATCHING"] = "true"
 import streamlit as st
 import pandas as pd
 import numpy as np
